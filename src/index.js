b86bd634a819c2487a67a6941d27e966a1914537
Noah Bartfield <noahbartfield@Noahs-MacBook-Pro.local>
2020-06-22 15:42:35 -0500

    colors randomizing and posting to firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase/app'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDM0ziu1IKBMgjGv_bFO_-QwYdGKCU90Tk",
+  authDomain: "colordatafeed.firebaseapp.com",
+  databaseURL: "https://colordatafeed.firebaseio.com",
+  projectId: "colordatafeed",
+  storageBucket: "colordatafeed.appspot.com",
+  messagingSenderId: "423920005187",
+  appId: "1:423920005187:web:a15c5a3773698f7c2f64f6",
+  measurementId: "G-D9R66XRTE6",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
