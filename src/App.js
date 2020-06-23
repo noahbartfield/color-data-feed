8a24a1fe826f9dc8515d0e1cffb832b55a055512
Daniel Clarke <deliotclarke@gmail.com>
2020-06-23 11:45:38 -0500

    removed css imports, but added again at the end. was getting a weird kleur error. edited colorBlock component. added chose color history to app js just to get some dynamically created colorBlocks on the screen.

--- src/App.js
+++ src/App.js
@@ -1,72 +1,104 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import * as firebase from 'firebase'
-import cleanData from './cleanData.js'
-import buildModel from './tfjs'
+import * as firebase from 'firebase';
+import cleanData from './cleanData.js';
+import buildModel from './tfjs';
+
+import ColorBlock from './colorBlock';
 
 function App() {
-
-  const [color, setColor] = useState(`rgb(${Math.floor((Math.random() * 255) + 1)}, ${Math.floor((Math.random() * 255) + 1)}, ${Math.floor((Math.random() * 255) + 1)})`)
-  const [R, setR] = useState('')
-  const [G, setG] = useState('')
-  const [B, setB] = useState('')
+  const [color, setColor] = useState(
+    `rgb(${Math.floor(Math.random() * 255 + 1)}, ${Math.floor(
+      Math.random() * 255 + 1
+    )}, ${Math.floor(Math.random() * 255 + 1)})`
+  );
+  const [R, setR] = useState('');
+  const [G, setG] = useState('');
+  const [B, setB] = useState('');
+  const [colorHistory, setColorHistory] = useState([]);
 
   function changeColor() {
-    let r = Math.floor((Math.random() * 255) + 1)
-    setR(r)
-    let g = Math.floor((Math.random() * 255) + 1)
-    setG(g)
-    let b = Math.floor((Math.random() * 255) + 1)
-    setB(b)
-    
-    setColor(`rgb(${r}, ${g}, ${b})`)
+    let r = Math.floor(Math.random() * 255 + 1);
+    setR(r);
+    let g = Math.floor(Math.random() * 255 + 1);
+    setG(g);
+    let b = Math.floor(Math.random() * 255 + 1);
+    setB(b);
+
+    setColor(`rgb(${r}, ${g}, ${b})`);
     console.log('backgroundColor: ', color);
   }
 
   function submit(button) {
     //send object to firebase and refresh page
-    console.log('submitting')
+    console.log('submitting');
 
-    let colorDatabase = firebase.database().ref('colors')
-    const data = ({
-      "R": R,
-      "G": G,
-      "B": B,
-      "label": button.target.id
-    })
+    let colorDatabase = firebase.database().ref('colors');
+    const data = {
+      R: R,
+      G: G,
+      B: B,
+      label: button.target.id,
+    };
 
     let color = colorDatabase.push(data);
-    console.log("Firebase generated key: " + color.key);
+    console.log('Firebase generated key: ' + color.key);
 
-    changeColor()
+    setColorHistory((colorHistory) => [...colorHistory, data]);
+
+    changeColor();
   }
 
   useEffect(() => {
-    changeColor()
-  }, [])
+    changeColor();
+  }, []);
 
   function logData() {
-    cleanData()
+    cleanData();
   }
 
-  buildModel()
+  buildModel();
 
   return (
-    <div className="App">
+    <div className='App'>
       <div>
-        <canvas style={{ background: color, height: '240px', width: '240px' }}></canvas>
+        <canvas
+          style={{ background: color, height: '240px', width: '240px' }}
+        ></canvas>
       </div>
-      <button id='red-ish' onClick={submit}>red-ish</button>
-      <button id='pink-ish' onClick={submit}>pink-ish</button>
-      <button id='purple-ish' onClick={submit}>purple-ish</button>
-      <button id='blue-ish' onClick={submit}>blue-ish</button>
-      <button id='green-ish' onClick={submit}>green-ish</button>
-      <button id='yellow-ish' onClick={submit}>yellow-ish</button>
-      <button id='orange-ish' onClick={submit}>orange-ish</button>
-      <button id='brown-ish' onClick={submit}>brown-ish</button>
-      <button id='grey-ish' onClick={submit}>grey-ish</button>
+      <button id='red-ish' onClick={submit}>
+        red-ish
+      </button>
+      <button id='pink-ish' onClick={submit}>
+        pink-ish
+      </button>
+      <button id='purple-ish' onClick={submit}>
+        purple-ish
+      </button>
+      <button id='blue-ish' onClick={submit}>
+        blue-ish
+      </button>
+      <button id='green-ish' onClick={submit}>
+        green-ish
+      </button>
+      <button id='yellow-ish' onClick={submit}>
+        yellow-ish
+      </button>
+      <button id='orange-ish' onClick={submit}>
+        orange-ish
+      </button>
+      <button id='brown-ish' onClick={submit}>
+        brown-ish
+      </button>
+      <button id='grey-ish' onClick={submit}>
+        grey-ish
+      </button>
       <div>
         <button onClick={logData}>Log Data</button>
+      </div>
+      <div style={{ display: 'flex', width: '100%', height: 'auto' }}>
+        {colorHistory.length > 0 &&
+          colorHistory.map((colorData) => <ColorBlock colorData={colorData} />)}
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import * as firebase from 'firebase/app'
+import * as firebase from 'firebase/app';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDM0ziu1IKBMgjGv_bFO_-QwYdGKCU90Tk",
-  authDomain: "colordatafeed.firebaseapp.com",
-  databaseURL: "https://colordatafeed.firebaseio.com",
-  projectId: "colordatafeed",
-  storageBucket: "colordatafeed.appspot.com",
-  messagingSenderId: "423920005187",
-  appId: "1:423920005187:web:a15c5a3773698f7c2f64f6",
-  measurementId: "G-D9R66XRTE6",
+  apiKey: 'AIzaSyDM0ziu1IKBMgjGv_bFO_-QwYdGKCU90Tk',
+  authDomain: 'colordatafeed.firebaseapp.com',
+  databaseURL: 'https://colordatafeed.firebaseio.com',
+  projectId: 'colordatafeed',
+  storageBucket: 'colordatafeed.appspot.com',
+  messagingSenderId: '423920005187',
+  appId: '1:423920005187:web:a15c5a3773698f7c2f64f6',
+  measurementId: 'G-D9R66XRTE6',
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
